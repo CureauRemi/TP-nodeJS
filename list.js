3a31b29be92e09c0108b7a5b0f80d081c69a585f
Remi Cureau <remicureau35@gmail.com>
2018-11-05 17:57:08 +0100

    listing, ajout, suppression terminés, il ne reste plus que la modification pour les trois catégories

--- list.js
+++ list.js
@@ -1,13 +1,70 @@
 const express = require ('express');
-
+const fs = require('fs');
 const router = express.Router();
 
+// composant : id, name, user, items
+//listing 
 router.get('/', (req, res) => {
-    res.send('All list');
-});
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+      if (err) {
+          console.error(err);
+          res.status(500).send(err);
+      } else {
+          console.log(data);
+          var obj = JSON.parse(data);
+          res.json(obj.list)
+      }
+    })
+  });
 
-router.get('/:items', (req, res) => {
-    res.send(`One list with id ${req.params.list}`);
-});
 
+//ajout
+router.post('/', (req, res) => {    
+    fs.readFile('./data.json', 'utf-8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send(err);
+        } else {
+            console.log("avant ajout : ", data);
+            var obj = JSON.parse(data);
+            obj.list.push({id:req.body.id, name:req.body.name, user:req.body.user, items:req.body.items});
+            var json = JSON.stringify(obj, null, 4);
+            fs.writeFile('data.json', json, 'utf8', (error) => {
+                if (err) {
+                    console.error(error);
+                    res.status(500).send(error);
+                } 
+                console.log("apres ajout :", data)
+                res.json(obj.list)
+            });
+        }
+    })
+  });
+  
+  // suppression d'un list
+  router.delete('/:id', (req,res) =>{
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+      if(err){
+        console.log(err);
+        res.status(500).send(err);
+      }
+      else{
+        console.log("avant suppression : " ,data);
+        var obj = JSON.parse(data);
+        obj.list = obj.list.filter(list => {
+          if(list.id === req.params.id) { return false } else { return true }
+        })
+        var json = JSON.stringify(obj, null, 4);
+        fs.writeFile('data.json', json, 'utf8', (error) => {
+          if (err) {
+              console.error(error);
+              res.status(500).send(error);
+          } 
+          console.log("apres suppression : ", data)
+          res.send(true);
+      });
+      }
+    })
+  })
+  
 module.exports = router;
--- user.js
+++ user.js
@@ -2,6 +2,8 @@ const express = require ('express');
 const fs = require('fs');
 const router = express.Router();
 
+
+//listirg 
 router.get('/', (req, res) => {
   fs.readFile('./data.json', 'utf-8', (err, data) => {
     if (err) {
@@ -22,7 +24,7 @@ router.post('/', (req, res) => {
           console.error(err);
           res.status(500).send(err);
       } else {
-          console.log(data);
+          console.log("avant ajout : ", data);
           var obj = JSON.parse(data);
           obj.user.push({id:req.body.id, name:req.body.name, password:req.body.password});
           var json = JSON.stringify(obj, null, 4);
@@ -31,6 +33,7 @@ router.post('/', (req, res) => {
                   console.error(error);
                   res.status(500).send(error);
               } 
+              console.log("apres ajout :", data)
               res.json(obj.user)
           });
       }
@@ -45,7 +48,7 @@ router.delete('/:id', (req,res) =>{
       res.status(500).send(err);
     }
     else{
-      console.log(data);
+      console.log("avant suppression : " ,data);
       var obj = JSON.parse(data);
       obj.user = obj.user.filter(user => {
         if(user.id === req.params.id) { return false } else { return true }
@@ -56,10 +59,27 @@ router.delete('/:id', (req,res) =>{
             console.error(error);
             res.status(500).send(error);
         } 
+        console.log("apres suppression : ", data)
         res.send(true);
     });
     }
   })
 })
+
+
+//modification d'un utilisateur
+router.put('/:id', (req,res) =>{
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+        if(err){
+          console.log(err);
+          res.status(500).send(err);
+        }
+        else{
+            console.log("avant modification", data)
+            var obj = JSON.parse(data);
+
+        }
+    })
+});
 
 module.exports = router;
